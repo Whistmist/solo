f910600ffc7fa3f0d523cc38e14294a89cea4466
mist <505168746@qq.com>
2019-07-23 18:05:20 +0800

    SpringCloud-Eureka注册中心

---
+++ eureka-server/src/main/java/com/hand/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.hand.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+/**
+ * @Author:  HuangHeng
+ * @Description  配置Eureka配置信息
+ * @CreateDate:  2019/7/23
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
