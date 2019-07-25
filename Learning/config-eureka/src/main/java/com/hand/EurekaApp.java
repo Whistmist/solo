a465c31623d9fb42a1a97f3cf7efb8102d128a13
mist <505168746@qq.com>
2019-07-25 15:53:24 +0800

    config-eureka

---
+++ Learning/config-eureka/src/main/java/com/hand/EurekaApp.java
@@ -0,0 +1,16 @@
+package com.hand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by HuangHeng on 2019/7/25
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApp {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApp.class, args);
+    }
+}
